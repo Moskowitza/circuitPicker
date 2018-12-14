309a2d828d2fc4250187dddda691c272f0a063ef
moskowitza <moskowitzaaron@mac.com>
2018-12-14 18:38:49 -0500

    form submission

--- circuit-picker/src/components/App.js
+++ circuit-picker/src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "./Header/Header";
 import Boulders from "./Boulders/Boulders";
 import Circuit from "./Circuit/Circuit";
 import AddEdit from "./AddEdit/AddEdit";
--- circuit-picker/src/components/GymPicker/GymPicker.js
+++ circuit-picker/src/components/GymPicker/GymPicker.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { getFunName } from "../../helpers";
 class GymPicker extends React.Component {
+  myInput = React.createRef();
+  goToGym = e => {
+    //prevent form from submitting
+    e.preventDefault();
+    console.log(this.myInput);
+    // get input text
+
+    // change page to the gym name
+  };
+  componentDidMount = () => console.log("mounted");
   render() {
     return (
       <>
-        <form className="gym-selector">
+        <form className="gym-selector" onSubmit={this.goToGym}>
           <h2>This is the Gym Picker Component</h2>
           <input
             type="text"
+            ref={this.myInput}
             required
             placeholder="Gym Name"
             defaultValue={getFunName()}
