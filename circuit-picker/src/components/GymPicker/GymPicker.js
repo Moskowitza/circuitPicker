7d4c07f5fe292f2b80b08499b00b8a9de8e1ec43
moskowitza <moskowitzaaron@mac.com>
2018-12-14 16:00:09 -0500

    Dropping in rando name

--- circuit-picker/src/components/GymPicker/GymPicker.js
+++ circuit-picker/src/components/GymPicker/GymPicker.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { getFunName } from "../../helpers";
 class GymPicker extends React.Component {
   render() {
     return (
       <>
         <form className="gym-selector">
           <h2>This is the Gym Picker Component</h2>
-          <input type="text" required placeholder="gym name" />
+          <input
+            type="text"
+            required
+            placeholder="Gym Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">visit gym</button>
         </form>
       </>
