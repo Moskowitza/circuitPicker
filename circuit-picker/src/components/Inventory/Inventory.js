cfa26073981c3a9b66000d1c4cb12e3b1476e9fb
moskowitza <moskowitzaaron@mac.com>
2018-12-26 08:47:51 -0500

    google auth

--- circuit-picker/src/components/Inventory/Inventory.js
+++ circuit-picker/src/components/Inventory/Inventory.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import firebase from "firebase";
+import { firebaseApp } from "../../base";
 import Header from "../Header/Header";
 import AddClimb from "../AddClimb/AddClimb";
 import EditClimb from "../EditClimb/EditClimb";
-import SignIn from "../SignIn/SignIn";
-var firebaseui = require("firebaseui");
+import Login from "../Login/Login";
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -18,74 +18,30 @@ class Inventory extends React.Component {
     uid: null,
     owner: null
   };
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.authHander({ user });
-      }
-    });
-  }
-  authHander = async authData => {
-    //look up current store in database
-    //claim it if there isn't one
-    //Set the state of the inventory Component to that of current user
+  authHandler = async authData => {
     console.log(authData);
   };
-
-  singIn = (email, password) => {
-    firebase
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
       .auth()
-      .signInWithEmailAndPassword(email, password)
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
   };
-  signOut = async () => {
-    await firebase
-      .auth()
-      .signOut()
-      .then(function() {
-        this.setState({ uid: null });
-      })
-      .catch(function(error) {
-        // An error happened.
-      });
-  };
-  start = () => {
-    const ui = new firebaseui.auth.AuthUI(firebase.auth());
-    ui.start("#firebaseui-auth-container", {
-      signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID]
-      // Other config options...
-    });
-  };
-
+  // componentDidMount() {
+  //   firebase.auth().onAuthStateChanged(user => {
+  //     if (user) {
+  //       this.authHander({ user });
+  //     }
+  //   });
+  // }
   render() {
-    const logout = <button onClick={this.signOut}>Sign Out</button>;
-
-    // check if they are logged in
-    if (!this.state.uid) {
-      return <button onClick={this.start}>Register</button>;
-    }
-    //if logged in, check if they are the Gym Owner
-    if (this.state.uid !== this.state.owner) {
-      return (
-        <div>
-          <p>You can't edit these routes</p>
-          <button onClick={this.signOut}>Sign Out</button>
-        </div>
-      );
-    }
     //otherwise, you're good.
+    return <Login authenticate={this.authenticate} />;
     return (
       <div className="section">
         <Header tagline="Add and Edit" />
         <h2>Inventory</h2>
-
         {Object.keys(this.props.climbs).map(key => (
           <EditClimb
             deleteClimb={this.props.deleteClimb}
@@ -97,7 +53,6 @@ class Inventory extends React.Component {
         ))}
         <AddClimb addClimb={this.props.addClimb} />
         <button onClick={this.props.loadSamples}>load Samples</button>
-        {logout}
       </div>
     );
   }
